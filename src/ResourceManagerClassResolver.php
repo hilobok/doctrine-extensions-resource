d7201b7e1576aecb43dc5f84954a16d7be4d69a6
Andrew Hilobok <hilobok@gmail.com>
2014-06-05 16:57:14 +0300

    Fixed unused code

--- src/ResourceManagerClassResolver.php
+++ src/ResourceManagerClassResolver.php
@@ -20,21 +20,17 @@ class ResourceManagerClassResolver
         switch (true) {
             case ($manager instanceof ORMObjectManager):
                 return $this->managerClasses['orm'];
-                break;
 
             case ($manager instanceof MongoDBObjectManager):
                 return $this->managerClasses['mongodb'];
-                break;
 
             case ($manager instanceof PHPCRObjectManager):
                 return $this->managerClasses['phpcr'];
-                break;
 
             default:
                 throw new \InvalidArgumentException(
                     sprintf("Unable to resolve manager class for '%s'.", get_class($manager))
                 );
-                break;
         }
     }
 
